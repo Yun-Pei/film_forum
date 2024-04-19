0ce38c0392a1968539466108e8eadc472ea30345
DESKTOP-30NIG86\Ariel <arielhuang0311.tw@gmail.com>
2024-04-19 18:51:55 +0800

    Create table(not suecess)

--- film_forum/film_forum/settings.py
+++ film_forum/film_forum/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'film_forum.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "forum",
+        "USER": "kounel",
+        "PASSWORD": "weloveHF",
+        "HOST": "goldenrain.tw",
+        "PORT": "3306",
     }
 }
 
