729413f5363b94f2b967a58cb7d5b68e8f8410a0
DESKTOP-30NIG86\Ariel <arielhuang0311.tw@gmail.com>
2024-04-20 12:03:46 +0800

    暫時先不連資料庫
    等server設定好

--- film_forum/film_forum/settings.py
+++ film_forum/film_forum/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'member'
 ]
 
 MIDDLEWARE = [
@@ -74,13 +75,9 @@ WSGI_APPLICATION = 'film_forum.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "forum",
-        "USER": "kounel",
-        "PASSWORD": "weloveHF",
-        "HOST": "goldenrain.tw",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
