5fdeda7ca807810c3555e1c40133fae59b58286b
DESKTOP-30NIG86\Ariel <arielhuang0311.tw@gmail.com>
2024-04-20 13:57:56 +0800

    連到phpmyadmin

--- film_forum/member/models.py
+++ film_forum/member/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 # Create your models here.
-# class Movies(models.Model):
-#     id = models.AutoField(primary_key=True)  # Field name made lowercase.
-#     name = models.CharField(max_length=255)  # Field name made lowercase.
-#     year = models.CharField(max_length=255)
-#     time = models.CharField(max_length=255)
-#     age = models.CharField(max_length=255)
-#     introduction = models.CharField(max_length=255)
-#     link = models.CharField(max_length=255)
-#     img = models.CharField(max_length=255)
-#     director = models.CharField(max_length=255)
-#     star = models.CharField(max_length=255)
-#     type = models.CharField(max_length=255)
+class Movies(models.Model):
+    id = models.AutoField(primary_key=True)  # Field name made lowercase.
+    name = models.CharField(max_length=255)  # Field name made lowercase.
+    year = models.CharField(max_length=255)
+    time = models.CharField(max_length=255)
+    age = models.CharField(max_length=255)
+    introduction = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    img = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    star = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
 
-#     class Meta:
-#         managed = True #代表需要Django幫你在資料庫建立這個table
-#         db_table = 'Movies' #資料庫內table的名字，預設會是django_space
+    class Meta:
+        managed = True #代表需要Django幫你在資料庫建立這個table
+        db_table = 'Movies' #資料庫內table的名字，預設會是django_space
