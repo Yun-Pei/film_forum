57a009a83e982a45c843fcf5eeafcedcf90be72e
rrrrrr0958 <bee19623@gmail.com>
2024-04-21 23:20:49 +0800

    增加聊天室背景圖

--- film_forum/chatroom/views.py
+++ film_forum/chatroom/views.py
@@ -2,12 +2,3 @@ from django.shortcuts import render
 #新加入的function
 def testPage(request):
     return render(request, "chatroom.html")
-
-def testPage(request):
-    i = [x for x in range(1,10)] #一個0~9的list
-    data = {
-        "data1":"資料1",
-        "data2":"資料2",
-        "list":i,
-    }
-    return render(request, "chatroom.html", data)
