e64a753f1b883d1f35db701d7105119620300d76
DESKTOP-30NIG86\Ariel <arielhuang0311.tw@gmail.com>
2024-04-25 13:45:25 +0800

    修改url.py
    將url.py的import home import views，改成import home import views as home_views

--- film_forum/film_forum/urls.py
+++ film_forum/film_forum/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home import views
+from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home", views.testPage),
+    path("home", home_views.testPage),
     # path("", views.index, name="home")
 ]
