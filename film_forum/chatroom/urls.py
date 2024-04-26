7f5b6933320b2004494c40deb9bd19ce93206ada
rrrrrr0958 <bee19623@gmail.com>
2024-04-27 04:03:44 +0800

    聊天室外觀小調整，離開的連結還是弄不好

--- film_forum/chatroom/urls.py
+++ film_forum/chatroom/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from chatroom import views as chat_views
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -6,7 +6,6 @@ urlpatterns = [
     path("", chat_views.chatPage, name="chat-page"),
 
     # authentication section
-    path("auth/login/", LoginView.as_view
-         (template_name="loginPage.html"), name="login-user"),
+    path("auth/login/", LoginView.as_view(template_name="loginPage.html"), name="login-user"),
     path("auth/logout/", LogoutView.as_view(), name="logout-user"),
 ]
