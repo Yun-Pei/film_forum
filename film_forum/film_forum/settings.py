754a5c71c950f00b34bad2cc6fc0529a43e83ea8
Gule7309 <bazinga73kook@gmail.com>
2024-04-27 20:28:53 +0800

    navbar加入其他的app

--- film_forum/film_forum/settings.py
+++ film_forum/film_forum/settings.py
@@ -132,7 +132,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "global_static",
 ]
 
 # Default primary key field type
--- film_forum/movie/views.py
+++ film_forum/movie/views.py
@@ -10,48 +10,48 @@ from django.utils import timezone
 
 @csrf_exempt
 def movie(request):
-    film = Movies.objects.filter(id=72).values_list("id", "name", "year", "time", "age", "introduction", "img", "director", "star", "type")
-    forum_article = Forums.objects.filter().order_by("-time").values('f_id', 'm_id', 'time', 'title', 'content', 'user_id')
+    # film = Movies.objects.filter(id=72).values_list("id", "name", "year", "time", "age", "introduction", "img", "director", "star", "type")
+    # forum_article = Forums.objects.filter().order_by("-time").values('f_id', 'm_id', 'time', 'title', 'content', 'user_id')
 
     # for article in forum_article:
     #     article.formatted_time = article.time.strftime("%Y-%m-%d %I:%M %p")
 
-    print(forum_article)
+    # print(forum_article)
 
     if request.method == "GET":
         # get movie information
         # print(film[0][0])
 
         # create a Forum
-        form = ForumsForm()
-
-    if request.method == 'POST':
-        # print("here")
-        form = ForumsForm(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data['title']
-            # print(title)
-
-            content = form.cleaned_data['content']
-            # print(content)
-
-            # m_id = form.cleaned_data['m_id']
-            film_id = Movies.objects.filter(id=72).values_list("id", flat=True).first()
-            # print(film_id)
-
-            now_time = timezone.now()
-            # print(now_time)
-
-            forum = Forums(title=title, content=content, m_id=film_id, time=now_time)
-            forum.save()
-
-            film = None
-
-            return redirect('movie')  # 導入路徑
-        # jsut test
-        else:
-            return redirect('forum')
-    # else:
     #     form = ForumsForm()
 
-    return render(request, "movie.html", {'form': form, 'film': film, 'forum_article': forum_article})
+    # if request.method == 'POST':
+    #     # print("here")
+    #     form = ForumsForm(request.POST)
+    #     if form.is_valid():
+    #         title = form.cleaned_data['title']
+    #         # print(title)
+
+    #         content = form.cleaned_data['content']
+    #         # print(content)
+
+    #         # m_id = form.cleaned_data['m_id']
+    #         film_id = Movies.objects.filter(id=72).values_list("id", flat=True).first()
+    #         # print(film_id)
+
+    #         now_time = timezone.now()
+    #         # print(now_time)
+
+    #         forum = Forums(title=title, content=content, m_id=film_id, time=now_time)
+    #         forum.save()
+
+    #         film = None
+
+    #         return redirect('movie')  # 導入路徑
+    #     # jsut test
+    #     else:
+    #         return redirect('forum')
+    # # else:
+    # #     form = ForumsForm()
+        return render(request, "movie.html")
+    # return render(request, "movie.html", {'form': form, 'film': film, 'forum_article': forum_article})
