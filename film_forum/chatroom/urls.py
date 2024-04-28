d9228c1bc1738cf8e38a9739be965b9153f6427f
rrrrrr0958 <bee19623@gmail.com>
2024-04-28 09:45:59 +0800

    新增聊天室搜尋使用者功能
    下一步要新增創建聊天室功能

--- film_forum/chatroom/urls.py
+++ film_forum/chatroom/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("auth/login/", LoginView.as_view(template_name="loginPage.html"), name="login-user"),
     path("auth/logout/", LogoutView.as_view(), name="logout-user"),
     path('auth/addChat/', views.addChatPage, name='add-Chat'),
+    path('searchMembers/', views.MemberSearchView.as_view(), name='search_members'),
+
 ]
--- film_forum/chatroom/views.py
+++ film_forum/chatroom/views.py
@@ -1,6 +1,8 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from member.models import *
-from django.db.models import Q
+from django.views.generic import ListView
 #新加入的function
 def testPage(request):
     return render(request, "chatroom.html")
@@ -13,3 +15,14 @@ def chatPage(request):
 
 def addChatPage(request):
     return render(request, 'addChatPage.html')
+
+class MemberSearchView(ListView):
+    model = User
+    template_name = 'addChatPage.html'
+    context_object_name = 'users'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return User.objects.filter(username__icontains=query)
+        return User.objects.none()
