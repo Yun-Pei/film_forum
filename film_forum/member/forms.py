26168da074fcf8932529e4ac6bc32f00cfd90ebf
DESKTOP-30NIG86\Ariel <arielhuang0311.tw@gmail.com>
2024-05-02 19:49:59 +0800

    修改User 的img 讓img可以為null(default=1)

--- film_forum/member/forms.py
+++ film_forum/member/forms.py
@@ -50,15 +50,15 @@ class RegisterForm(UserCreationForm):
             }
         )
     )
-    img = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control"
-            }
-        )
-    )
+    # img = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control"
+    #         }
+    #     )
+    # )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'img')
-
+        fields = ('username', 'email', 'password1', 'password2')
+# , 'img'
