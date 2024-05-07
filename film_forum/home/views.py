bdb6465d20a7f59fc0792540e63ac3382ca48f9d
Gule7309 <bazinga73kook@gmail.com>
2024-05-07 21:21:42 +0800

    完成存瀏覽紀錄和觀看數排行榜
    可修正的:點一次A電影Browsed會存2筆一樣的資料但不同時間。(對於計算觀看數無所謂，因為每個都是2筆)

--- film_forum/home/views.py
+++ film_forum/home/views.py
@@ -9,44 +9,20 @@ from django.db.models import Count
 #新加入的function
 def testPage(request):
 
-    movies1 = Movies.objects.filter(year__gt=2019)[:10]
     movies2 = Movies.objects.filter(year__gt=2019)[:10]
     movies3 = Movies.objects.filter(year__gt=2019)[:10]
     movieup = Movies.objects.filter(mid=89)
 
-    # top_movies = Browse.objects.values('mid').annotate(num_views=Count('mid')).order_by('-num_views')[:10]
-    # top_movie_ids = [entry['mid'] for entry in top_movies]
-    # movies1 = Movies.objects.filter(mid__in=top_movie_ids)
-    
+    top_movies = Browse.objects.values('mid').annotate(num_views=Count('mid')).order_by('-num_views')[:10]
+    top_movie_ids = [entry['mid'] for entry in top_movies]
+    movies1 = Movies.objects.filter(mid__in=top_movie_ids)
+    print(movies1)
     if request.GET.get("term"):
         term = request.GET.get('term')
         movies = Movies.objects.filter(name__icontains=term)[:20]
         
         data = [{'label': movie.name, 'value': movie.name, 'url': str(movie.mid) } for movie in movies]
         return JsonResponse(data, safe=False)
-    
-    print(request.GET)
-    movie_id = request.GET.get('m_id') 
-    print('below is mid')
-    print(movie_id)
-    
-    if movie_id:
-
-        if request.user.is_authenticated:
-            user_id = request.user.id
-            try:
-                user = User.objects.get(pk=user_id)
-                film = Movies.objects.get(pk=movie_id)
-                browse_time = timezone.now()
-                browse = Browse(uid=user, mid=film, browseTime=browse_time)
-                browse.save()
-                print("Browse entry saved successfully!")
-            except User.DoesNotExist:
-                print("User does not exist")
-            except Movies.DoesNotExist:
-                print("Movie does not exist")
-            except Exception as e:
-                print(f"An error occurred while saving browse entry: {e}")
         
     return render(request, "index.html", {'movies1': movies1, 'movies2': movies2, 'movies3': movies3, 'movieup': movieup[0]}) 
 
