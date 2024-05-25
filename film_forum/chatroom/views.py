a58943cdb12c34d4205ee1420abefed2a124abc3
rrrrrr0958 <bee19623@gmail.com>
2024-05-26 07:47:26 +0800

    基本上概念都做到了
     現在剩一些判斷的部分還有我要怎麼控制websocket

--- film_forum/chatroom/views.py
+++ film_forum/chatroom/views.py
@@ -9,6 +9,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import csrf_protect
 from django.db import connection
+from django.utils import timezone
 
 #新加入的function
 def dictfetchall(cursor):
@@ -31,37 +32,96 @@ def chatPage(request):
 
     chatlist = list()
     with connection.cursor() as cursor:
-        # cursor.execute("""
-        #     SELECT *
-        #     FROM Chatroom
-        #     JOIN User ON User.id = Chatroom.be_uid
-        #     JOIN Message ON Message.aid_id = Chatroom.aid
-        #     WHERE uid_id = %s
-        #     ORDER BY Message.time DESC
-        # """, [user_id])
         cursor.execute("""
             SELECT *
             FROM Chatroom
             JOIN User ON User.id = Chatroom.be_uid
+            JOIN (
+                SELECT aid_id, MAX(time) AS latest_time
+                FROM Message
+                GROUP BY aid_id
+            ) AS LatestMessage ON LatestMessage.aid_id = Chatroom.aid
+            JOIN Message ON Message.aid_id = LatestMessage.aid_id AND Message.time = LatestMessage.latest_time
             WHERE uid_id = %s
+            ORDER BY Message.time DESC
         """, [user_id])
         chatroom = dictfetchall(cursor)
         chatlist.append(chatroom)
         print(user_id, chatlist)
 
-    messagelist = list()
+    Nochatlist = list()
     with connection.cursor() as cursor:
         cursor.execute("""
-            SELECT Message.time, Message.conent
-            FROM Message
-            JOIN Chatroom ON Chatroom.uid_id = Message.aid_id
-            WHERE Message.aid_id = %s
-            ORDER BY Message.time
-        """, [user_id])
-        messageResult = dictfetchall(cursor)
-        messagelist.append(messageResult)
+            SELECT *
+            FROM Chatroom AS C1
+            JOIN User ON User.id = C1.be_uid
+            WHERE C1.uid_id = %s
+            AND NOT EXISTS (
+                SELECT 1
+                FROM Chatroom AS C2
+                JOIN Message ON Message.aid_id = C2.aid
+                WHERE C2.uid_id = %s
+                AND C1.aid = C2.aid
+            )
+        """, [user_id, user_id])
+        Nochatroom = dictfetchall(cursor)
+        Nochatlist.append(Nochatroom)
 
-    return render(request, "chatPage.html", {'chatlist': chatlist, 'messagelist': messagelist})
+    
+
+
+    # messagelist = list()
+    # with connection.cursor() as cursor:
+    #     cursor.execute("""
+    #         SELECT Message.time, Message.conent
+    #         FROM Message
+    #         JOIN Chatroom ON Chatroom.aid = Message.aid_id
+    #         WHERE Message.aid_id = %s
+    #         ORDER BY Message.time
+    #     """, [user_id])
+    #     messageResult = dictfetchall(cursor)
+    #     messagelist.append(messageResult)
+    #     print(messagelist)
+    # messagelist = list()
+    if request.method == 'POST':
+        if request.POST.get('mode') == 'sendMessage':
+            time = timezone.now()
+            content = request.POST.get('messageContent')
+            chatroom_id = request.POST.get('aid')
+            # print(user_id, time, content, chatroom_id)
+            NewMessage = Message(time=time, conent=content, aid_id=chatroom_id)
+            NewMessage.save()
+            return HttpResponseRedirect('/chat/')
+        elif request.POST.get('mode') == 'sendAid':
+            chatroom_aid = request.POST.get('aid')
+            chatroomUid = request.POST.get('chatroomUid')
+            chatroomBe = request.POST.get('chatroomBe')
+            # messagelist = list()
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT Chatroom.uid_id, Chatroom.be_uid, Message.conent, DATE_FORMAT(Message.time, '%%Y/%%m/%%d %%H:%%i') AS formatted_datetime
+                    FROM Message
+                    JOIN Chatroom ON Chatroom.aid = Message.aid_id
+                    WHERE (Chatroom.uid_id = %s AND Chatroom.be_uid = %s) OR (Chatroom.uid_id = %s AND Chatroom.be_uid = %s)
+                    ORDER BY Message.time
+                """, [chatroomBe, chatroomUid, chatroomUid, chatroomBe])
+                message_result = cursor.fetchall()
+            # 將訊息數據轉換為 JSON 格式
+            messages = []
+            for row in message_result:
+                message = {
+                    'uid': row[0],
+                    'beuid': row[1],
+                    'content': row[2],  # 設置為您的 content 欄位
+                    'time': row[3],     # 設置為您的 time 欄位
+                    # 添加其他欄位...
+                }
+                messages.append(message)
+            return JsonResponse(messages, safe=False)
+
+    # print(messagelist)      
+    # return render(request, "chatPage.html", {'chatlist': chatlist, 'message_result': message_result, 'Nochatlist':Nochatlist})
+    return render(request, "chatPage.html", {'chatlist': chatlist, 'Nochatlist':Nochatlist})
 
 @csrf_protect
 def addChatPage(request):
